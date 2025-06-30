8534681f06b42d619420e5cdc5fd9ad169534e8c
Manas-Kanwal <hs79957@gmail.com>
2025-06-30 11:22:11 +0000

    recursion

---
+++ recursion/Linerasearch.java
@@ -0,0 +1,25 @@
+public class Linerasearch {
+    
+    static boolean Linearsearch(int arr [] , int target , int idx )
+    {
+        if(idx>=arr.length)
+        {
+            return false ;
+        }
+
+        
+
+        if(arr[idx] == target)
+        {
+            return true ;
+        }
+
+        return Linearsearch(arr, target, idx+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,0};
+        int target = 1 ;
+        int i = 0 ;
+        System.out.println(Linearsearch(arr , target , i));
+    }
+}
